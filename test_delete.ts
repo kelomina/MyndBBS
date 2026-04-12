57f26ce1d8cdb82472f90e8d3157d4a3a770c1dc
kelomina <kelomina@users.noreply.github.com>
2026-04-12 04:09:35 +0000

    feat: 移除冗余代码，增强复用率
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- test_delete.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-
-async function test() {
-  const msg = await prisma.privateMessage.findFirst();
-  console.log('msg', msg?.id);
-}
-test().finally(() => prisma.$disconnect());
--- test_roles.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-async function test() {
-  const roles = await prisma.role.findMany();
-  console.log(roles.map(r => r.name));
-}
-test().finally(() => prisma.$disconnect());
