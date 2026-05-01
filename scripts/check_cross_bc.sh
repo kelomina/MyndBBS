57cdd8b736df4e767b06645808be9cb5c4b88ca9
kelomina <kelomina@users.noreply.github.com>
2026-05-01 16:07:00 +0800

    移除过期脚本

--- scripts/check_cross_bc.sh
+++
@@ -1,4 +0,0 @@
-for bc in community identity messaging notification provisioning system; do
-  echo "Checking $bc..."
-  grep -rn -E "import.*from.*(community|identity|messaging|notification|provisioning|system)" ./packages/backend/src/application/$bc | grep -v "$bc"
-done
